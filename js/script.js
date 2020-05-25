422a8e5640501030ecadac433260ac444408c65b
Ali Ourrami <ourrami97@gmail.com>
2020-05-25 04:16:54 +0000

    add class active when scrolling

--- js/script.js
+++ js/script.js
@@ -66,3 +66,37 @@ function scrollFunction() {
         header.classList.remove("nav-scrolled");
     }
 }
+
+//
+
+let mainNavLinks = document.querySelectorAll(".item ul li a");
+let mainSections = document.querySelectorAll(".content-main section");
+
+let lastId;
+let cur = [];
+
+// This should probably be throttled.
+// Especially because it triggers during smooth scrolling.
+// https://lodash.com/docs/4.17.10#throttle
+// You could do like...
+// window.addEventListener("scroll", () => {
+//    _.throttle(doThatStuff, 100);
+// });
+// Only not doing it here to keep this Pen dependency-free.
+
+window.addEventListener("scroll", event => {
+    let fromTop = window.scrollY;
+
+    mainNavLinks.forEach(link => {
+        let section = document.querySelector(link.hash);
+
+        if (
+            section.offsetTop <= fromTop &&
+            section.offsetTop + section.offsetHeight > fromTop
+        ) {
+            link.classList.add("active");
+        } else {
+            link.classList.remove("active");
+        }
+    });
+});
